ca32a833585184a6531637dfc03ee60895317aed
dinhhuy123 <huycholon93@gmail.com>
2022-09-13 19:36:59 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+
+const route = require('./routes');
+const db = require('./config/db')
+
+// Connect to DB
+db.connect();
+
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,11 +35,11 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Route init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`App listening on port http://localhost:${port}`);
 });
